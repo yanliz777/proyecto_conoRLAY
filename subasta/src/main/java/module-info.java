bed77952795796426a919aaab6c09b7941158a03
yanliz777 <111096374+yanliz777@users.noreply.github.com>
2023-12-13 15:54:58 -0500

    realización del login

--- subasta/src/main/java/module-info.java
+++ subasta/src/main/java/module-info.java
@@ -1,8 +1,12 @@
-module co.edu.uniquindio.subasta {
+module co.edu.uniquindio.subasta//maneja toda la parte gráfica del proyecto
+{
     requires javafx.controls;
     requires javafx.fxml;
 
 
     opens co.edu.uniquindio.subasta to javafx.fxml;
     exports co.edu.uniquindio.subasta;
+    opens co.edu.uniquindio.viewController to javafx.fxml;
+    exports co.edu.uniquindio.viewController;
+
 }
