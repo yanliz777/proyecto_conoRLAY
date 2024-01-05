6d2a554efce446dff5dfa937963a55e7fe43ce44
yanliz777 <111096374+yanliz777@users.noreply.github.com>
2024-01-05 15:56:05 -0500

    Se modifico el modelo agregando el tipo de ususario en la clase Persona, también se realizo el login dependiendo el usuario, es decir: vendedor,cliente o administrador

--- subasta/src/main/java/co/edu/uniquindio/model/Subasta.java
+++ subasta/src/main/java/co/edu/uniquindio/model/Subasta.java
@@ -1,11 +1,63 @@
 package co.edu.uniquindio.model;
 
+import java.util.ArrayList;
+
 public class Subasta
 {
+    Administrador administrador;
+    private ArrayList<Vendedor> vendedores;
+    private ArrayList<Cliente> clientes;
+    //constructor:
     public  Subasta(){}
+    public ArrayList<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public ArrayList<Vendedor> getVendedores()
+    {
+        return vendedores;
+    }
 
     public boolean iniciarSesion(String cedula,String password)
     {
-        return  true;
+        boolean bandera = false;
+        return  bandera;
+    }
+
+    public boolean iniciarSesionCliente(String cedula, String password) {
+        boolean bandera = false;
+        for (Cliente cliente : getClientes())
+        {
+            if (cliente.getCedula().equals(cedula) &&
+                    cliente.getContraseña().equals(password))
+            {
+                bandera = true;
+                break;
+            }
+        }
+        return  bandera;
+    }
+
+    public boolean iniciarSesionVendedor(String cedula, String password) {
+        boolean bandera = false;
+        for (Vendedor vendedor : getVendedores())
+        {
+            if (vendedor.getCedula().equals(cedula) &&
+                    vendedor.getContraseña().equals(password))
+            {
+                bandera = true;
+                break;
+            }
+        }
+        return  bandera;
+    }
+
+    public boolean iniciarSesionAdministrador(String cedula, String password) {
+        boolean bandera = false;
+        if(administrador.getCedula().equals(cedula) &&
+                administrador.getPersona().equals(password)) {
+            bandera = true;
+        }
+        return  bandera;
     }
 }
