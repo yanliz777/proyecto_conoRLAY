74e6f08d0c82c89be699629a6e50f7bd6f69e712
yanliz777 <111096374+yanliz777@users.noreply.github.com>
2024-01-11 13:11:11 -0500

    Se modificó la clase ModelFactory y Subasta

--- subasta/src/main/java/co/edu/uniquindio/controller/ModelFactoryController.java
+++ subasta/src/main/java/co/edu/uniquindio/controller/ModelFactoryController.java
@@ -1,8 +1,12 @@
 package co.edu.uniquindio.controller;
 
+import co.edu.uniquindio.model.Cliente;
 import co.edu.uniquindio.model.Subasta;
+import co.edu.uniquindio.model.Vendedor;
 import co.edu.uniquindio.utils.Persistencia;
 import co.edu.uniquindio.utils.subastaUtil;
+
+import java.util.ArrayList;
 
 public class ModelFactoryController
 {
@@ -33,8 +37,59 @@ public class ModelFactoryController
         return subasta;
     }
 
+    /*
+    Método que me permite validar los datos a qué Objeto
+    pertenecen, puede ser cliente o vendedor
+     */
     public boolean iniciarSesion(String cedula, String password) {
-        return getSubasta().iniciarSesion(cedula,password);
+        boolean bandera = false;
+        if (validarSesionVendedor(subasta.getVendedores(),cedula,password))
+        {
+            bandera = true;
+        }
+        else if(validarSesionCliente(subasta.getClientes(),cedula,password))
+        {
+            bandera = true;
+        }
+        else{
+            System.out.println("ERROR!!!!");
+        }
+        return bandera;
+        //return getSubasta().iniciarSesion(cedula,password);
+    }
+
+    /**
+     *Método que me permite validar los datos de inicio de sección con los
+     * objetos Tipo Cliente
+     */
+    public boolean validarSesionCliente(ArrayList<Cliente> lista,String cedula,String password)
+    {
+        boolean bandera = false;
+        for (Cliente cliente : subasta.getClientes())
+        {
+            if (cliente.getCedula().equals(cedula) &&
+                    cliente.getContraseña().equals(password))
+            {
+                bandera = true;
+                break;
+            }
+        }
+        return bandera;
+    }
+
+    public boolean validarSesionVendedor(ArrayList<Vendedor> lista, String cedula, String password)
+    {
+        boolean bandera = false;
+        for (Vendedor vendedor: subasta.getVendedores())
+        {
+            if (vendedor.getCedula().equals(cedula) &&
+                    vendedor.getContraseña().equals(password))
+            {
+                bandera = true;
+                break;
+            }
+        }
+        return bandera;
     }
 
     public void cargarDatosBase()
